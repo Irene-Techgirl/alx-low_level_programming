6c2173f947c5c872df254d48505751735e5103ba
Irene Agbani <ireneagbani64@gmail.com>
2023-04-28 21:43:27 -0700

    4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print num except 2 and 4
+ * return: void
+ */
+void print_most_numbers(void)
+{
+	char c
+
+	for (C = '0'; c <= '9; C++)
+	{
+		if (I(c == '2' (l c == '49))
+			_putchar(c);
+	}	
+	_putchar('\n');
+}
