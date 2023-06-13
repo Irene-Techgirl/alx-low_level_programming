7849dfa363b30fcf0395f14d8df40c614ed98748
Irene Agbani <ireneagbani64@gmail.com>
2023-06-13 15:46:46 -0700

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,41 +1,33 @@
-#include <fcntl.h>
-#include <unistd.h>
 #include "main.h"
-#include <stdlib.h>
-#include <sys/stat.h>
 
 /**
- * create_file - Creates a file with the given filename and writes the
- *               text_content to it.
- * @filename: Name of the file to create.
- * @text_content: NULL-terminated string to write to the file.
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: 1 on success, -1 on failure.
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor, write_result, text_length = 0;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (file_descriptor == -1)
-		return (-1);
-
 	if (text_content != NULL)
 	{
-		while (text_content[text_length] != '\0')
-			text_length++;
-
-		write_result = write(file_descriptor, text_content, text_length);
-		if (write_result == -1)
-		{
-			close(file_descriptor);
-			return (-1);
-		}
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	close(file_descriptor);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
 	return (1);
 }
